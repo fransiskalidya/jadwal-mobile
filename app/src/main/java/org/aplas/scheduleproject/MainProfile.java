fc6617abe562310ad675d85d1ae08ee3faa02ae9
ardilalukita14 <lukitasari623@gmail.com>
2021-10-22 19:47:34 +0700

    modifikasi intent

--- app/src/main/java/org/aplas/scheduleproject/MainProfile.java
+++ app/src/main/java/org/aplas/scheduleproject/MainProfile.java
@@ -1,6 +1,12 @@
 package org.aplas.scheduleproject;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
 
 public class MainProfile extends Activity {
+    public void moveToChangePassword(View view) {
+        Intent intent = new Intent(MainProfile.this, MainUbahPassword.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/org/aplas/scheduleproject/MainRegister.java
+++ app/src/main/java/org/aplas/scheduleproject/MainRegister.java
@@ -1,6 +1,12 @@
 package org.aplas.scheduleproject;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
 
 public class MainRegister extends Activity {
+    public void moveToDashboard(View view) {
+        Intent intent = new Intent(MainRegister.this, Dashboard.class);
+        startActivity(intent);
+    }
 }
